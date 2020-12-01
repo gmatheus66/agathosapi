4209941084d9a154bc6eb3ab062fa6ab1f3af631
Matheus <goncalves@usto.re>
2020-11-30 22:58:28 -0300

    Start Project

---
+++ src/main/java/com/api/agathos/AgathosApplication.java
@@ -0,0 +1,13 @@
+package com.api.agathos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgathosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgathosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/agathos/AgathosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.agathos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgathosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
